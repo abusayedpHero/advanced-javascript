4a11a450aa5109059a7e5e29167aa6b253aee9a3
abusayedpHero <developer.abu88@gmail.com>
2022-07-22 09:59:22 +0600

    null vs undefiend cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+let pakhi;
+
+// function er vitore kono kisu return na korle and value set na korle undefined hobe.;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+// kono akta paramiter 2ta value ke pass na korle undefined hobe:::
+
+function add (num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+
+const premik = {name:"smart dude", phone:6365579};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
